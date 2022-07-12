eb67fa1d6ed68f67f3b9e8d1a43258879c6e1938
Rohan Gupta <97931981+simplyrohan@users.noreply.github.com>
2022-07-12 15:23:06 -0700

    setup.py v0.0.1

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='pyspritex',
+    version='0.0.1',
+    packages=['pyspritex'],
+    url='https://github.com/simplyrohan/pyspritex',
+    license='MIT License',
+    author='Rohan Gupta',
+    author_email='',
+    description='About A package to make desktop applications easy to develop in python'
+)
